15d5872c0a0f5c59dbb1a7d8149aa60db9707ccc
aryaverma-01 <aryaverma740@gmail.com>
2025-05-25 21:38:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Query
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
-marks_dict = {
+marks_data = {
     "Alice": 85,
     "Bob": 90,
-    "Charlie": 78,
-    "David": 92,
+    "X": 10,
+    "Y": 20
 }
 
 @app.get("/api")
-def get_marks(name: list[str] = Query([])):
-    results = [marks_dict.get(n, 0) for n in name]
+def get_marks(name: list[str] = Query(...)):
+    results = [marks_data.get(n, 0) for n in name]
     return {"marks": results}
